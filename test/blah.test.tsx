c5fbfe22b94bc9c0b9758f5061aa8519345d0904
Agustin N. R. Ramirez <agustin.n.r.ramirez@gmail.com>
2019-07-05 01:54:28 -0300

    Fix test

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { MuiForm } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<MuiForm schema={{}} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
